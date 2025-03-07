120117559dcb261332c44a226d9ccfb49099a9ec
rsrxrsr <36836051+rsrxrsr@users.noreply.github.com>
2025-03-07 07:30:23 +0000

    Pending changes exported from your codespace

---
+++ myapp/src/app/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private http = inject(HttpClient);
+  private apiUrl = 'https://effective-space-chainsaw-j49r5grgqvqcpp47-8080.app.github.dev/entity/usuario'; // Ejemplo de API
+
+  getData(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+}
